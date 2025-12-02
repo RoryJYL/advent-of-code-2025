f38dd210b0f4ede27a7ea30f77a164f399d4436e
Rory <jyljensen@outlook.com>
2025-12-02 20:25:17 +0800

    first two days

---
+++ 1.js
@@ -0,0 +1,56 @@
+import { readFileSync } from "node:fs";
+
+const MAX = 100;
+
+function solution1() {
+  const input = readFileSync("input/1.txt", "utf8");
+  const lines = input.split("\n");
+  let cur = 50;
+  let res = 0;
+  for (const line of lines) {
+    const dir = line[0];
+    const dis = Number(line.slice(1));
+    if (dir === "L") {
+      cur = (cur - dis + MAX) % MAX;
+    } else {
+      cur = (cur + dis) % MAX;
+    }
+
+    if (cur === 0) {
+      res += 1;
+    }
+  }
+
+  return res;
+}
+
+function solution2() {
+  const input = readFileSync("input/1.txt", "utf8");
+  const lines = input.split("\n");
+  let cur = 50;
+  let res = 0;
+  for (const line of lines) {
+    const atZero = cur === 0;
+    const dir = line[0];
+    const orgDis = Number(line.slice(1));
+    const circles = Math.floor(orgDis / MAX);
+    const dis = orgDis % MAX;
+    if (dir === "L") {
+      cur = cur - dis;
+    } else {
+      cur = cur + dis;
+    }
+
+    res += circles;
+    if (!atZero && (cur <= 0 || cur >= MAX)) {
+      res += 1;
+    }
+
+    cur = (cur + MAX) % MAX;
+  }
+
+  return res;
+}
+
+console.log(solution1());
+console.log(solution2());
---
+++ 2.js
@@ -0,0 +1,66 @@
+import { readFileSync } from "node:fs";
+
+function solution1() {
+  const input = readFileSync("input/2.txt", "utf8");
+  const intervals = input.split(",");
+
+  let res = 0;
+
+  for (const interval of intervals) {
+    const [start, end] = interval.split("-").map(Number);
+    for (let i = start; i <= end; i += 1) {
+      const str = `${i}`;
+      const length = str.length;
+      if (length % 2 !== 0) {
+        continue;
+      }
+
+      const mid = length / 2;
+      if (str.slice(0, mid) === str.slice(mid)) {
+        res += i;
+      }
+    }
+  }
+
+  return res;
+}
+
+function solution2() {
+  const input = readFileSync("input/2.txt", "utf8");
+  const intervals = input.split(",");
+
+  let res = 0;
+
+  for (const interval of intervals) {
+    const [start, end] = interval.split("-").map(Number);
+    for (let i = start; i <= end; i += 1) {
+      if (findPattern(i)) {
+        res += i;
+      }
+    }
+  }
+
+  return res;
+}
+
+function findPattern(id) {
+  const str = `${id}`;
+  const length = str.length;
+  const mid = Math.floor(length / 2);
+
+  for (let i = mid; i > 0; i -= 1) {
+    if (length % i !== 0) {
+      continue;
+    }
+
+    const times = length / i;
+    if (str.slice(0, i).repeat(times) === str) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+console.log(solution1());
+console.log(solution2());
